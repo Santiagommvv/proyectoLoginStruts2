de0387003d620c68bd2b8cde24d41664ed33ed41
Santiago Morales <58920781+Santiagommvv@users.noreply.github.com>
2023-04-10 17:34:53 -0300

    subida del proyecto base

---
+++ src/main/java/com/beans/actions/ValidarUsuarioAction.java
@@ -0,0 +1,67 @@
+package com.beans.actions;
+
+import static com.opensymphony.xwork2.Action.*;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.logging.log4j.*;
+import org.apache.struts2.convention.annotation.*;
+
+@Results({
+    @Result(name="success", location="/WEB-INF/content/bienvenido.jsp"),
+    @Result(name="input", location="/WEB-INF/content/login.jsp")
+})
+public class ValidarUsuarioAction extends ActionSupport{
+    
+    Logger log = LogManager.getLogger(LoginAction.class);
+    
+    private String usuario;
+    private String password;
+
+    @Action("validarUsuario")
+    public String execute(){
+        if("admin".equals(this.usuario)){
+            addActionMessage(getText("usuario.valido"));
+            return SUCCESS;
+        }
+        return INPUT;
+    }
+    
+    @Override
+    public void validate(){
+        if(this.usuario == null || "".equals(this.usuario.trim())){
+            //1er param -> name="usuario" en login.jsp
+            addFieldError("usuario", getText("val.usuario"));
+        }
+        else if(!usuarioValido()){
+            addActionError(getText("usuario.invalido"));
+        }
+        
+        if(this.password == null || "".equals(this.password.trim())){
+            //1er param -> name="password" en login.jsp
+            addFieldError("password", getText("val.password"));
+        }
+        else if(this.password.length() < 3){
+            addFieldError("password", getText("val.pass.min.length"));
+        }
+    }
+    
+    private boolean usuarioValido(){
+        return "admin".equals(this.usuario);
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
